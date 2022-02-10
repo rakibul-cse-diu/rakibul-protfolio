7429e32136f7c7a28bdf7f13eba071709faa2406
rakibul-cse-diu <rakibul2180@gmail.com>
2022-02-10 17:24:45 +0600

    add toggle nav bar menu

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 // variables 
 const openMenu = document.getElementById("menu-open");
 const closeMenu = document.getElementById("menu-close");
-const navBar = document.querySelector(".nav-bar");
-const menuItems = document.querySelector(".menu-items");
+const menuItems = document.getElementById("menu-items");
 
 
 // Toggle open handle
 function handleMenuOpen() {
-    openMenu.style.display = "none";
-    navBar.classList.remove("navbar-toggle");
+    menuItems.style.right = "0px";
 }
 
 
 // Toggle close handle
 function handleMenuClose() {
-    openMenu.style.display = "block";
-    navBar.classList.add("navbar-toggle");
+    menuItems.style.right = "-3000px";
 }
