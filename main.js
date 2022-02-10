a39c13da4a3a1188f4a3e42ebca6f2f70187bcce
rakibul-cse-diu <rakibul2180@gmail.com>
2022-02-10 23:35:00 +0600

    add quote section

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const openMenu = document.getElementById("menu-open");
 const closeMenu = document.getElementById("menu-close");
 const menuItems = document.getElementById("menu-items");
+const preLoader = document.querySelector(".preloader");
+
+
+
+// On load preloader handle 
+function handlePreLoader() {
+    preLoader.style.display = "none";
+}
+
 
 
 // Toggle open handle
